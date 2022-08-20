5ce3c2ffd36880809f408e3fb9c8ea233a04a8d7
seoilgun <seoilgun@gmail.com>
2022-08-20 10:41:35 +0900

    dd

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -16,28 +16,30 @@ def main(args):
     training_corpus = get_training_corpus(corpus_lines)
 
     old_tokenizer = AutoTokenizer.from_pretrained("hyunwoongko/asian-bart-en")
-    # print(old_tokenizer)
+    print(old_tokenizer)
     # old_xlmroberta_tokenizer = XLMRobertaTokenizer.from_pretrained("hyunwoongko/asian-bart-en")
     # print(old_xlmroberta_tokenizer)
     # old_mbart_tokenizer = MBartTokenizer.from_pretrained("hyunwoongko/asian-bart-en")
     # print(old_mbart_tokenizer)
-    # old_tokenizer.add_tokens(['<tr>', '<td>'] + ['<tdcolspan="{}">'.format(i) for i in range(10)] + ['<tdrowspan="{}">'.format(i) for i in range(10)])
+    new_tokens = ['<tr>', '<td>'] + ['<tdcolspan="{}">'.format(i) for i in range(10)] + ['<tdrowspan="{}">'.format(i) for i in range(10)]
+    # old_tokenizer.add_tokens(new_tokens)
     # print("added tokens", old_tokenizer)
     print("training!")
     # print("old_tokenizer.vocab_size", old_tokenizer.vocab_size)
-    tokenizer = old_tokenizer.train_new_from_iterator(training_corpus, old_tokenizer.vocab_size)# + 22)  # 52000)
-
+    tokenizer = old_tokenizer.train_new_from_iterator(training_corpus, old_tokenizer.vocab_size + 22, new_special_tokens=new_tokens)  # 52000)
+    print(tokenizer.encode("<tr><td>test<tr><td>"))
     os.makedirs(args.output_dir, exist_ok=True)
     tokenizer.save_pretrained(args.output_dir)
 
     loaded_tokenizer = AutoTokenizer.from_pretrained(args.output_dir)
     print(loaded_tokenizer)
-
+    print(loaded_tokenizer.encode("<tr><td>test<tr><td>"))
     # xlmroberta_tokenizer = XLMRobertaTokenizer.from_pretrained(args.output_dir)
     # print(xlmroberta_tokenizer)
 
     mbart_tokenizer = MBartTokenizer.from_pretrained(args.output_dir)
     print(mbart_tokenizer)
+    print(mbart_tokenizer.encode("<tr><td>test<tr><td>"))
     print("done")
 
 
@@ -45,6 +47,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--corpus_path', type=str,
                         default="D:\corpus.txt")
-    parser.add_argument('--output_dir', type=str, default="D:\dataset/table_ocr/tokenizer")
+    parser.add_argument('--output_dir', type=str, default="D:\dataset/table_ocr/tokenizer2")
 
     main(parser.parse_args())
