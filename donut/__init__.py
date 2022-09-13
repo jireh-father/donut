5820931e4da0d296b2be11758879cea9e8e93d41
seoilgun <seoilgun@gmail.com>
2022-09-13 14:32:08 +0900

    `ls`

--- donut/__init__.py
+++ donut/__init__.py
@@ -4,12 +4,13 @@ Copyright (c) 2022-present NAVER Corp.
 MIT License
 """
 from .model import DonutConfig, DonutModel, DonutClipModel, DonutClipConfig
-from .util import DonutDataset, JSONParseEvaluator, load_json, save_json
+from .util import DonutDataset, JSONParseEvaluator, load_json, save_json, DonutClipDataset
 
 __all__ = [
     "DonutConfig",
     "DonutModel",
     "DonutDataset",
+    "DonutClipDataset",
     "JSONParseEvaluator",
     "load_json",
     "save_json",
