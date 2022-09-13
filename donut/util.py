74674b11cb7e2e8e34f8ef05d38cb2056adc5f3f
seoilgun <seoilgun@gmail.com>
2022-09-13 14:28:55 +0900

    `ls`

--- donut/util.py
+++ donut/util.py
@@ -41,15 +41,15 @@ class DonutDataset(Dataset):
     """
 
     def __init__(
-        self,
-        dataset_name_or_path: str,
-        donut_model: PreTrainedModel,
-        max_length: int,
-        split: str = "train",
-        ignore_id: int = -100,
-        task_start_token: str = "<s>",
-        prompt_end_token: str = None,
-        sort_json_key: bool = True,
+            self,
+            dataset_name_or_path: str,
+            donut_model: PreTrainedModel,
+            max_length: int,
+            split: str = "train",
+            ignore_id: int = -100,
+            task_start_token: str = "<s>",
+            prompt_end_token: str = None,
+            sort_json_key: bool = True,
     ):
         super().__init__()
 
@@ -61,14 +61,10 @@ class DonutDataset(Dataset):
         self.prompt_end_token = prompt_end_token if prompt_end_token else task_start_token
         self.sort_json_key = sort_json_key
         self.dataset_name_or_path = dataset_name_or_path
-        self.dataset = load_dataset(os.path.join(dataset_name_or_path, self.split), data_files='metadata.jsonl')['train']
+        self.dataset = load_dataset(os.path.join(dataset_name_or_path, self.split), data_files='metadata.jsonl')[
+            'train']
         self.dataset_length = len(self.dataset)
         self.gt_token_sequences = []
-
-        if hasattr(self.donut_model, "decoder"):
-            text_model = self.donut_model.decoder
-        else:
-            text_model = self.donut_model.text_encoder
 
         for sample in self.dataset:
             ground_truth = json.loads(sample["ground_truth"])
@@ -87,13 +83,13 @@ class DonutDataset(Dataset):
                         update_special_tokens_for_json_key=self.split == "train",
                         sort_json_key=self.sort_json_key,
                     )
-                    + text_model.tokenizer.eos_token
+                    + self.donut_model.decoder.tokenizer.eos_token
                     for gt_json in gt_jsons  # load json from list of json
                 ]
             )
 
-        text_model.add_special_tokens([self.task_start_token, self.prompt_end_token])
-        self.prompt_end_token_id = text_model.tokenizer.convert_tokens_to_ids(self.prompt_end_token)
+        self.donut_model.decoder.add_special_tokens([self.task_start_token, self.prompt_end_token])
+        self.prompt_end_token_id = self.donut_model.decoder.tokenizer.convert_tokens_to_ids(self.prompt_end_token)
 
     def __len__(self) -> int:
         return self.dataset_length
@@ -129,9 +125,117 @@ class DonutDataset(Dataset):
             labels = input_ids.clone()
             labels[
                 labels == self.donut_model.decoder.tokenizer.pad_token_id
-            ] = self.ignore_id  # model doesn't need to predict pad token
+                ] = self.ignore_id  # model doesn't need to predict pad token
             labels[
-                : torch.nonzero(labels == self.prompt_end_token_id).sum() + 1
+            : torch.nonzero(labels == self.prompt_end_token_id).sum() + 1
+            ] = self.ignore_id  # model doesn't need to predict prompt (for VQA)
+            return input_tensor, input_ids, labels
+        else:
+            prompt_end_index = torch.nonzero(
+                input_ids == self.prompt_end_token_id
+            ).sum()  # return prompt end index instead of target output labels
+            return input_tensor, input_ids, prompt_end_index, processed_parse
+
+
+class DonutClipDataset(Dataset):
+    """
+    DonutDataset which is saved in huggingface datasets format. (see details in https://huggingface.co/docs/datasets)
+    Each row, consists of image path(png/jpg/jpeg) and gt data (json/jsonl/txt),
+    and it will be converted into input_tensor(vectorized image) and input_ids(tokenized string).
+
+    Args:
+        dataset_name_or_path: name of dataset (available at huggingface.co/datasets) or the path containing image files and metadata.jsonl
+        ignore_id: ignore_index for torch.nn.CrossEntropyLoss
+        task_start_token: the special token to be fed to the decoder to conduct the target task
+    """
+
+    def __init__(
+            self,
+            dataset_name_or_path: str,
+            donut_model: PreTrainedModel,
+            max_length: int,
+            split: str = "train",
+            ignore_id: int = -100,
+            task_start_token: str = "<s>",
+            prompt_end_token: str = None,
+            sort_json_key: bool = True,
+    ):
+        super().__init__()
+
+        self.donut_model = donut_model
+        self.max_length = max_length
+        self.split = split
+        self.ignore_id = ignore_id
+        self.task_start_token = task_start_token
+        self.prompt_end_token = prompt_end_token if prompt_end_token else task_start_token
+        self.sort_json_key = sort_json_key
+        self.dataset_name_or_path = dataset_name_or_path
+        self.dataset = load_dataset(os.path.join(dataset_name_or_path, self.split), data_files='metadata.jsonl')[
+            'train']
+        self.dataset_length = len(self.dataset)
+        self.gt_token_sequences = []
+
+        for sample in self.dataset:
+            ground_truth = json.loads(sample["ground_truth"])
+            if "gt_parses" in ground_truth:  # when multiple ground truths are available, e.g., docvqa
+                assert isinstance(ground_truth["gt_parses"], list)
+                gt_jsons = ground_truth["gt_parses"]
+            else:
+                assert "gt_parse" in ground_truth and isinstance(ground_truth["gt_parse"], dict)
+                gt_jsons = [ground_truth["gt_parse"]]
+
+            self.gt_token_sequences.append(
+                [
+                    task_start_token
+                    + self.donut_model.json2token(
+                        gt_json,
+                        update_special_tokens_for_json_key=self.split == "train",
+                        sort_json_key=self.sort_json_key,
+                    )
+                    + self.donut_model.text_encoder.tokenizer.eos_token
+                    for gt_json in gt_jsons  # load json from list of json
+                ]
+            )
+
+        self.prompt_end_token_id = self.donut_model.text_encoder.tokenizer.convert_tokens_to_ids(self.prompt_end_token)
+
+    def __len__(self) -> int:
+        return self.dataset_length
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Load image from image_path of given dataset_path and convert into input_tensor and labels
+        Convert gt data into input_ids (tokenized string)
+
+        Returns:
+            input_tensor : preprocessed image
+            input_ids : tokenized gt_data
+            labels : masked labels (model doesn't need to predict prompt and pad token)
+        """
+        sample = self.dataset[idx]
+        im = Image.open(os.path.join(self.dataset_name_or_path, self.split, sample["file_name"]))
+        # input_tensor
+        # input_tensor = self.donut_model.encoder.prepare_input(sample["image"], random_padding=self.split == "train")
+        input_tensor = self.donut_model.image_encoder.prepare_input(im, random_padding=self.split == "train")
+
+        # input_ids
+        processed_parse = random.choice(self.gt_token_sequences[idx])  # can be more than one, e.g., DocVQA Task 1
+        input_ids = self.donut_model.text_encoder.tokenizer(
+            processed_parse,
+            add_special_tokens=False,
+            max_length=self.max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )["input_ids"].squeeze(0)
+
+        if self.split == "train":
+            labels = input_ids.clone()
+            labels[
+                labels == self.donut_model.text_encoder.tokenizer.pad_token_id
+                ] = self.ignore_id  # model doesn't need to predict pad token
+            labels[
+            : torch.nonzero(labels == self.prompt_end_token_id).sum() + 1
             ] = self.ignore_id  # model doesn't need to predict prompt (for VQA)
             return input_tensor, input_ids, labels
         else:
@@ -273,23 +377,23 @@ class JSONParseEvaluator:
             0,
             1
             - (
-                zss.distance(
-                    pred,
-                    answer,
-                    get_children=zss.Node.get_children,
-                    insert_cost=self.insert_and_remove_cost,
-                    remove_cost=self.insert_and_remove_cost,
-                    update_cost=self.update_cost,
-                    return_operations=False,
-                )
-                / zss.distance(
-                    self.construct_tree_from_dict(self.normalize_dict({})),
-                    answer,
-                    get_children=zss.Node.get_children,
-                    insert_cost=self.insert_and_remove_cost,
-                    remove_cost=self.insert_and_remove_cost,
-                    update_cost=self.update_cost,
-                    return_operations=False,
-                )
+                    zss.distance(
+                        pred,
+                        answer,
+                        get_children=zss.Node.get_children,
+                        insert_cost=self.insert_and_remove_cost,
+                        remove_cost=self.insert_and_remove_cost,
+                        update_cost=self.update_cost,
+                        return_operations=False,
+                    )
+                    / zss.distance(
+                self.construct_tree_from_dict(self.normalize_dict({})),
+                answer,
+                get_children=zss.Node.get_children,
+                insert_cost=self.insert_and_remove_cost,
+                remove_cost=self.insert_and_remove_cost,
+                update_cost=self.update_cost,
+                return_operations=False,
+            )
             ),
         )
