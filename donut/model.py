6b872059e008c4de7d5da4c1120850ebae4fd3b2
seoilgun <seoilgun@gmail.com>
2022-10-16 13:32:22 +0900

    `ls`

--- donut/model.py
+++ donut/model.py
@@ -920,7 +920,10 @@ class DonutClipModel(PreTrainedModel):
         """
         if type(obj) == dict:
             if len(obj) == 1 and "text_sequence" in obj:
-                return obj["text_sequence"]
+                text_sequence = obj["text_sequence"]
+                if text_sequence.startswith("<table>"):
+                    text_sequence = text_sequence[7:-8]
+                return text_sequence
             else:
                 output = ""
                 if sort_json_key:
