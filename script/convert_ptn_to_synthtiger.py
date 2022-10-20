46af2389ecdefe054d0b97ad0e629d5d4452bb56
seoilgun <seoilgun@gmail.com>
2022-10-20 18:59:28 +0900

    `ls`

--- script/convert_ptn_to_synthtiger.py
+++ script/convert_ptn_to_synthtiger.py
@@ -108,6 +108,10 @@ def main(args):
 
         if max_cols < nums_col:
             max_cols = nums_col
+        print("nums_row", nums_row)
+        print("nums_col", nums_col)
+        print("tmp_max_row_span", tmp_max_row_span)
+        print("tmp_max_col_span", tmp_max_col_span)
 
         print("max_row_span", max_row_span)
         print("max_col_span", max_col_span)
