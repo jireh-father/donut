0fc0c14041a1d61be63b5aba5b1937ab9e71b655
seoilgun <seoilgun@gmail.com>
2022-10-20 18:39:27 +0900

    `ls`

--- script/convert_ptn_to_synthtiger.py
+++ script/convert_ptn_to_synthtiger.py
@@ -6,6 +6,7 @@ from io import BytesIO
 import base64
 import json
 import cv2
+from bs4 import BeautifulSoup
 
 
 def remove_html_tags(text):
@@ -27,40 +28,16 @@ def convert_ptn_item_to_simple_html(item):
     total_texts = []
     i = 0
     tags = []
-    max_col_span = 0
-    max_row_span = 0
-    nums_row = 0
-    nums_col = 0
     while i < len(item['html']['structure']['tokens']):
 
         tag = item['html']['structure']['tokens'][i]
         tag = tag.strip()
         i += 1
-        if tag == "<tr>":
-            nums_row += 1
-        # if use_thead:
-        #     if tag.startswith("</t"):
-        #         continue
-        # else:
-        #     if tag in ["<thead>", "</thead>", "<tbody>", "</tbody>"] or tag.startswith("</t"):
-        #         continue
         if tag == "<td":
-            split_tag = item['html']['structure']['tokens'][i].strip().split('"')
-            num_spans = int(split_tag[1])
-            if "col" in split_tag[0]:
-                if num_spans > max_col_span:
-                    max_col_span = num_spans
-            else:
-                if num_spans > max_row_span:
-                    max_row_span = num_spans
-
             tag += item['html']['structure']['tokens'][i] + item['html']['structure']['tokens'][i + 1]
             i += 2
             tags.append(tag.strip())
-            nums_col += num_spans
         else:
-            if tag == "<td>":
-                nums_col += 1
             tags.append(tag)
 
     i = 0
@@ -74,7 +51,8 @@ def convert_ptn_item_to_simple_html(item):
                 text_set.update(set(text))
                 total_texts.append(text)
             i += 1
-    return table_tag, total_texts, text_set, max_row_span, max_col_span, nums_row, int(nums_col / nums_row)
+
+    return table_tag, total_texts, text_set
 
 
 def main(args):
@@ -86,8 +64,7 @@ def main(args):
         if args.test_cnt and i >= args.test_cnt:
             break
         item = json.loads(line)
-        table_tag, tmp_total_texts, text_set, tmp_max_row_span, tmp_max_col_span, nums_row, nums_col = convert_ptn_item_to_simple_html(
-            item)
+        table_tag, tmp_total_texts, text_set = convert_ptn_item_to_simple_html(item)
         file_name = item['filename']
         if item['split'] == "train":
             image_path = os.path.join(args.image_dir, "train", file_name)
@@ -96,14 +73,38 @@ def main(args):
         im = cv2.imread(image_path)
         height, width = im.shape[:2]
 
+        html = "<table>{}</table>".format("".join(table_tag))
+        bs = BeautifulSoup(html, 'html.parser')
+        trs = bs.find_all("tr")
+        nums_row = len(trs)
+        max_col = 0
+        for tr in trs:
+            for tds in tr.find_all("td"):
+                if max_col < len(tds):
+                    max_col = len(tds)
+        nums_col = max_col
+
+        td_rowspans = bs.select('td[rowspan]')
+        tmp_max_row_span = 0
+        for td_rowspan in td_rowspans:
+            if tmp_max_row_span < td_rowspan['rowspan']:
+                tmp_max_row_span = td_rowspan['rowspan']
+
+        td_colspans = bs.select('td[colspan]')
+        tmp_max_col_span = 0
+        for td_colspan in td_colspans:
+            if tmp_max_col_span < td_colspan['colspan']:
+                tmp_max_col_span = td_colspan['colspan']
+
         if max_row_span < tmp_max_row_span:
             max_row_span = tmp_max_row_span
         if max_col_span < tmp_max_col_span:
             max_col_span = tmp_max_col_span
+
         result_item = {
             'nums_col': nums_col,
             'nums_row': nums_row,
-            'html': "<table>{}</table>".format("".join(table_tag)),
+            'html': html,
             'width': width,
             'height': height,
             "has_span": tmp_max_row_span > 1 or tmp_max_col_span > 1,
