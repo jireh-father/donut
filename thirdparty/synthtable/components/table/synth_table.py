5b1a909e25f473208ae81564a043ea3d58470d83
seoilgun <seoilgun@gmail.com>
2022-10-21 11:34:16 +0900

    `ls`

--- thirdparty/synthtable/components/table/synth_table.py
+++ thirdparty/synthtable/components/table/synth_table.py
@@ -282,6 +282,7 @@ class SynthTable(Component):
                         print(json.dumps(self.meta))
                         traceback.print_exc()
                         print(table_row_span_map)
+                        print("ridx", ridx, "cidx", cidx, "real_cidx", real_cidx)
                         raise e
 
                     has_row_span = td_tag.has_attr('rowspan')
