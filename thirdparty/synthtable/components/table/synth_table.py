71cb54fc93a31b2cc6adbae7da6b67e4c824dceb
seoilgun <seoilgun@gmail.com>
2022-10-21 11:16:00 +0900

    `ls`

--- thirdparty/synthtable/components/table/synth_table.py
+++ thirdparty/synthtable/components/table/synth_table.py
@@ -272,8 +272,14 @@ class SynthTable(Component):
             td_tags = tr_element.find_all("td")
             for cidx, td_tag in enumerate(td_tags):
                 if self.meta['span']:
-                    while table_row_span_map[ridx][real_cidx]:
-                        real_cidx += 1
+                    try:
+                        while table_row_span_map[ridx][real_cidx]:
+                            real_cidx += 1
+                    except Exception as e:
+                        del self.meta['html_bs']
+                        print(self.meta)
+                        print(json.dumps(self.meta))
+                        raise e
 
                     has_row_span = td_tag.has_attr('rowspan')
                     has_col_span = td_tag.has_attr('colspan')
