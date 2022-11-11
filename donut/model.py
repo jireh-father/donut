45fa7a6cfea385e8df6be8529fe86a9bea67d52b
seoilgun <seoilgun@gmail.com>
2022-11-11 17:56:43 +0900

    `ls`

--- donut/model.py
+++ donut/model.py
@@ -606,8 +606,10 @@ class DonutModel(PreTrainedModel):
 
         output = {"predictions": list()}
         for seq in self.decoder.tokenizer.batch_decode(decoder_output.sequences):
+            print("before", seq)
             seq = seq.replace(self.decoder.tokenizer.eos_token, "").replace(self.decoder.tokenizer.pad_token, "")
             seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+            print("after", seq)
             if return_json:
                 output["predictions"].append(self.token2json(seq))
             else:
