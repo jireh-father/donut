594b010911b15af349423c2a591e20e4b7acf34e
seoilgun <seoilgun@gmail.com>
2022-11-30 20:48:41 +0900

    gogol

--- thirdparty/synthtable/components/table/synth_table.py
+++ thirdparty/synthtable/components/table/synth_table.py
@@ -26,7 +26,7 @@ english_regex = re.compile(
 latin_number_regex = re.compile(
     r'[\u0030-\u0039\u0041-\u005A\u0061-\u007A]')
 
-except_regex = re.compile(r'[\uE000–\uF8FF]')
+except_regex = re.compile(r'[\uE000–\uF8FF]', re.UNICODE)
 except_korean_english_regex = re.compile(
     r'[^\u0000-\u007E\u00A1-\u00BF\u2160-\u216B\u2170-\u217B\u2190-\u2199\u2200-\u22FF\u2460-\u2473\u24B6-\u24E9\uAC00-\uD7A3\u1100-\u11FF\u3131-\u318F\u0030-\u0039\u0041-\u005A\u0061-\u007A\u00D7\u00F7\u203B\u2022]')
 
