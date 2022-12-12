24d444cc358dfb371ffe365c79a3dce6923d200f
seoilgun <seoilgun@gmail.com>
2022-12-12 23:50:38 +0900

    `ls`

--- donut/model.py
+++ donut/model.py
@@ -610,6 +610,7 @@ class DonutModel(PreTrainedModel):
         print(decoder_output.keys())
         print(decoder_output.scores[0])
         print(decoder_output.scores[0].shape)
+        print(decoder_output.scores[1].shape)
         for seq in self.decoder.tokenizer.batch_decode(decoder_output.sequences):
             seq = seq.replace(self.decoder.tokenizer.eos_token, "").replace(self.decoder.tokenizer.pad_token, "")
             seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
