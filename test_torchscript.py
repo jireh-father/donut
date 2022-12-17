da23ce5688e97df49e43cfa63bc20bbc19b89bbc
seoilgun <seoilgun@gmail.com>
2022-12-17 15:53:27 +0900

    `ls`

--- test_torchscript.py
+++ test_torchscript.py
@@ -5,13 +5,15 @@ import os
 from torch.utils.mobile_optimizer import optimize_for_mobile
 from donut import DonutModel, JSONParseEvaluator, load_json, save_json, DonutConfig
 from sconf import Config
-
+import time
 
 def main(args, left_argv):
     model = torch.jit.load(args.model_path)
     config = Config(args.config)
     config.argv_update(left_argv)
+    start = time.time()
     ret = model(torch.rand(1, 3, config.input_size[0], config.input_size[1]))
+    print(time.time() - start)
     print(ret)
 
     print("done")
